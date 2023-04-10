257322bd928522f5b83aa68b44dd4a1cdca5f14a
Balaks0702 <gayathriks0702@gmail.com>
2023-04-10 01:43:44 -0400

    punny

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("click", function(event) {
     // Checking if the button was clicked
     if (!event.target.matches("#button")) return;
 
-    fetch("https://v2.jokeapi.dev/joke/Programming")
+    fetch("https://v2.jokeapi.dev/joke/Pun?blacklistFlags=nsfw,racist,sexist")
         .then((response) => response.json())
         .then((data) => renderJoke(data))
 });
